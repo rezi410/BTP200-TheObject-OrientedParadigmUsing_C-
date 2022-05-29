63c4e1641a76bbd000eb7028585f415e9553479a
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 10:24:49 -0400

    Update HtmlText.cpp

--- workshops/ws10/lab/HtmlText.cpp
+++ workshops/ws10/lab/HtmlText.cpp
@@ -1,11 +1,4 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Workshop:       Workshop10_LAB
-//==============================================
-// HtmlText.cpp
+
 
 #include <iostream>
 #include "HtmlText.h"
