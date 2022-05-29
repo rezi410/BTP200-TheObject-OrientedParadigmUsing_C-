b112ab916dd2ac93de2f34ceba46ec5447150899
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 10:26:09 -0400

    Update cstring.h

--- workshops/ws10/lab/cstring.h
+++ workshops/ws10/lab/cstring.h
@@ -1,11 +1,4 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Workshop:       Workshop10_LAB
-//==============================================
-// cstring.h
+
 
 #ifndef SDDS_CSTRING_H_
 #define SDDS_CSTRING_H_
