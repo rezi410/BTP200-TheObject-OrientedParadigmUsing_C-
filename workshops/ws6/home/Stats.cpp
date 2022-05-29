711129b1c80ae493bd8fbe491c6673fbbb519fec
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 09:52:56 -0400

    Update Stats.cpp

--- workshops/ws6/home/Stats.cpp
+++ workshops/ws6/home/Stats.cpp
@@ -1,10 +1,4 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Workshop:       Workshop6_DIY
-//==============================================
+
 // Stats.cpp
 
 #include <iostream>
