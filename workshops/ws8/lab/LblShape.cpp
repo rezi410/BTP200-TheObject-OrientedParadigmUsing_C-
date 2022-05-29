9700758c4d57d02d345205e5da8d1b5c0969c454
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 10:11:54 -0400

    Update LblShape.cpp

--- workshops/ws8/lab/LblShape.cpp
+++ workshops/ws8/lab/LblShape.cpp
@@ -1,11 +1,3 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Workshop:       Workshop8_LAB
-//==============================================
-// LblShape.cpp
 
 #include <iostream>
 #include <cstring>
