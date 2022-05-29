12cb9082c1eff87d8639d9a02e2a773a8d99e081
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 10:13:04 -0400

    Update Rectangle.cpp

--- workshops/ws8/lab/Rectangle.cpp
+++ workshops/ws8/lab/Rectangle.cpp
@@ -1,11 +1,4 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Workshop:       Workshop8_LAB
-//==============================================
-// Rectangle.cpp
+
 
 #include <iostream>
 #include <cstring>
