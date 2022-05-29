09b8602606bb29a87cd2465ec18afff72b331ca1
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 10:13:18 -0400

    Update Rectangle.h

--- workshops/ws8/lab/Rectangle.h
+++ workshops/ws8/lab/Rectangle.h
@@ -1,10 +1,4 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Workshop:       Workshop8_LAB
-//==============================================
+
 // Rectangle.h
 
 #ifndef SDDS_RECTANGLE_H_
